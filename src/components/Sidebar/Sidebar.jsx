8654c047e08ed140b1d5156c1c1794caeb1bd491
IDPogolovkin <96434412+IDPogolovkin@users.noreply.github.com>
2023-05-20 21:59:20 +0600

    qwertyui

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -89,7 +89,7 @@ const Sidebar = () => {
             )
             }
 
-           {user && is_teacher == True ? <div
+           {user && is_teacher == true ? <div
               onClick={() => routeHandler('/profileTeacher')}
               className={
                 window.location.pathname === '/profileTeacher'
@@ -100,7 +100,7 @@ const Sidebar = () => {
               <Sprite id="profile" />
               <h1>Профиль Учителя</h1>
             </div> : <div></div>}
-            {user && is_student == True ? <div
+            {user && is_student == true ? <div
               onClick={() => routeHandler('/profile')}
               className={
                 window.location.pathname === '/profile'
