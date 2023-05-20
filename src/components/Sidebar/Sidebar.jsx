8bb32adfc306149ed0879a746d1acb0349bbaecd
IDPogolovkin <96434412+IDPogolovkin@users.noreply.github.com>
2023-05-20 22:18:04 +0600

    ternary

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -89,7 +89,7 @@ const Sidebar = () => {
             )
             }
 
-           {user && is_teacher == true ? <div
+           {user && is_teacher ? <div
               onClick={() => routeHandler('/profileTeacher')}
               className={
                 window.location.pathname === '/profileTeacher'
@@ -100,7 +100,7 @@ const Sidebar = () => {
               <Sprite id="profile" />
               <h1>Профиль Учителя</h1>
             </div> : <div></div>}
-            {user && is_student == true ? <div
+            {user && is_student ? <div
               onClick={() => routeHandler('/profile')}
               className={
                 window.location.pathname === '/profile'
