af8472cfe1993b7136fac5db779ddf056bcc0f77
Timur <daniilpogolovkin@gmail.com>
2023-06-14 18:19:29 +0600

    sdf

--- src/components/SignInComponent/SignIn.jsx
+++ src/components/SignInComponent/SignIn.jsx
@@ -20,7 +20,7 @@ const SignIn = () => {
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const loggedIn = searchParams.get('loggedIn');
+  const loggedIn = searchParams.get('loggedIn') === 'True' ? 'True' : 'False';
   
   const navigate = useNavigate();
   const routeHandler = (URL) => {
