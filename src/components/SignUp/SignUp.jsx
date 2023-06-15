2bf1f540f9482cd9455eca2ed1bdb04c47168523
Timur <timur.grig.kz.2002@gmail.com>
2023-06-15 22:35:16 +0600

    avatar

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -7,6 +7,7 @@ import Sidebar from '../Sidebar/Sidebar';
 import { useNavigate } from 'react-router-dom'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { equal } from 'assert';
 
 
 const SignUp = () => {
@@ -20,7 +21,6 @@ const SignUp = () => {
     password: '',
     password2: '',
     telegram: '',
-    image: null
   });
 
   const [registrationStatus, setRegistrationStatus] = useState(null); // Registration status state
@@ -31,8 +31,15 @@ const SignUp = () => {
     navigate(URL)
   }
 
+  const [image, setImage] = useState(null);
+
   const handleChange = event => {
     const { name, value, type, checked } = event.target;
+    if ([event.target.name == 'image']) {
+      setImage({ 
+        image: event.target.files
+      });
+    }
     setFormData(prevFormData => ({
       ...prevFormData,
       [name]: type === 'checkbox' ? checked : value
@@ -44,14 +51,31 @@ const SignUp = () => {
     if (formData.password !== formData.password2) {
       setRegistrationStatus('error: Пароли не совпадают');
     } else {
-      axiosInstance
-        .post('signup/student/', formData)
-        .then(() =>{
-          setRegistrationStatus('success: Регистрация прошла успешно! Подтвердите вашу почту.');
-        })
-        .catch((error) => {
-          setRegistrationStatus(`error: ${error.message}`);
-        });
+
+      let fomatData = new FormData();
+      fomatData.append('first_name', formData.first_name);
+      fomatData.append('last_name', formData.last_name);
+      fomatData.append('surname', formData.surname);
+      fomatData.append('birth_date', formData.birth_date);
+      fomatData.append('email', formData.email);
+      fomatData.append('phone', formData.phone);
+      fomatData.append('password', formData.password);
+      fomatData.append('telegram', formData.telegram);
+      fomatData.append('image', image.image);
+
+      axiosInstance.post('signup/student/', fomatData).then(() => {
+        setRegistrationStatus('success: Регистрация прошла успешно! Подтвердите вашу почту.');
+      }).catch((error) => {
+        setRegistrationStatus(`error: ${error.message}`);
+      })
+      // axiosInstance
+      //   .post('signup/student/', formData)
+      //   .then(() => {
+      //     setRegistrationStatus('success: Регистрация прошла успешно! Подтвердите вашу почту.');
+      //   })
+      //   .catch((error) => {
+      //     setRegistrationStatus(`error: ${error.message}`);
+      //   });
     }
   };
 
@@ -60,10 +84,10 @@ const SignUp = () => {
   };
 
   return (
-    
+
     <div className="main">
-      <Sidebar/>
-        <form className="form" onSubmit={handleSubmit} encType="multipart/form-data">
+      <Sidebar />
+      <form className="form" onSubmit={handleSubmit} encType="multipart/form-data">
         <h1 className='regTitle'>Регистрация ученика</h1>
         <div className="form-buttons">
           <button onClick={() => routeHandler('/regTeach')} className="form--submit" type="button">
@@ -73,103 +97,103 @@ const SignUp = () => {
             Регистрация ученика
           </button>
         </div>
-      {/* <button onClick={() => routeHandler('/regEduCent')} className="form--submit" type="submit">
+        {/* <button onClick={() => routeHandler('/regEduCent')} className="form--submit" type="submit">
         Регистрация образ. центра
       </button> */}
         <p>Информация о ребенке</p>
         <div className="input-row">
-        <input
-          type="text"
-          placeholder="Имя"
-          name="first_name"
-          className="form--input"
-          value={formData.first_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Фамилия"
-          name="last_name"
-          className="form--input"
-          value={formData.last_name}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            placeholder="Имя"
+            name="first_name"
+            className="form--input"
+            value={formData.first_name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Фамилия"
+            name="last_name"
+            className="form--input"
+            value={formData.last_name}
+            onChange={handleChange}
+          />
         </div>
         <div className="input-row">
-        <input
-          type="text"
-          placeholder="Отчество"
-          name="surname"
-          className="form--input"
-          value={formData.surname}
-          onChange={handleChange}
-        />
-        <input
-          type="date"
-          placeholder="Дата рождения"
-          name="birth_date"
-          className="form--input"
-          value={formData.birth_date}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            placeholder="Отчество"
+            name="surname"
+            className="form--input"
+            value={formData.surname}
+            onChange={handleChange}
+          />
+          <input
+            type="date"
+            placeholder="Дата рождения"
+            name="birth_date"
+            className="form--input"
+            value={formData.birth_date}
+            onChange={handleChange}
+          />
         </div>
         <p>Контакты</p>
         <div className="input-row">
-        <input
-          type="email"
-          placeholder="Email адрес"
-          name="email"
-          className="form--input"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Номер Телефона"
-          name="phone"
-          className="form--input"
-          value={formData.phone}
-          onChange={handleChange}
-        />
+          <input
+            type="email"
+            placeholder="Email адрес"
+            name="email"
+            className="form--input"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Номер Телефона"
+            name="phone"
+            className="form--input"
+            value={formData.phone}
+            onChange={handleChange}
+          />
         </div>
         <p>Telegram ( Дополнительно )</p>
         <div className="input-row">
-        <input
-          type="text"
-          placeholder="Имя пользователя в Telegram"
-          name="telegram"
-          className="form--input-tg"
-          value={formData.telegram}
-          onChange={handleChange}
-        />
-        <input
-          accept='image/*'
-          type="file"
-          placeholder="Установи сука фото"
-          name="image"
-          className="form--input-tg"
-          value={formData.image}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            placeholder="Имя пользователя в Telegram"
+            name="telegram"
+            className="form--input-tg"
+            value={formData.telegram}
+            onChange={handleChange}
+          />
+          <input
+            accept='image/*'
+            type="file"
+            placeholder="Установи сука фото"
+            name="image"
+            className="form--input-tg"
+            value={formData.image}
+            onChange={handleChange}
+          />
         </div>
         <p>Установите пароль</p>
         <div className="input-row">
-        <input
-          type="password"
-          placeholder="Введите пароль"
-          name="password"
-          className="form--input"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Подтвердите пароль"
-          name="password2"
-          className="form--input"
-          value={formData.password2}
-          onChange={handleChange}
-        />
+          <input
+            type="password"
+            placeholder="Введите пароль"
+            name="password"
+            className="form--input"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Подтвердите пароль"
+            name="password2"
+            className="form--input"
+            value={formData.password2}
+            onChange={handleChange}
+          />
         </div>
         <br />
         <button className="form--submit-last" type="submit" onClick={handleSubmit} >
@@ -179,17 +203,17 @@ const SignUp = () => {
 
       {/* Registration status modal */}
       <Modal show={registrationStatus !== null} onHide={handleModalClose}>
-          <Modal.Body>
-            {registrationStatus && registrationStatus.startsWith('error') ? (
-              <p className="error-message">{registrationStatus.substr(7)}</p>
-            ) : registrationStatus && registrationStatus.startsWith('success') ? (
-              <p className="success-message">{registrationStatus.substr(9)}</p>
-            ) : null}
-            <Button variant="secondary" onClick={handleModalClose} className="close-button">
-              Закрыть
-            </Button>
-          </Modal.Body>
-        </Modal>
+        <Modal.Body>
+          {registrationStatus && registrationStatus.startsWith('error') ? (
+            <p className="error-message">{registrationStatus.substr(7)}</p>
+          ) : registrationStatus && registrationStatus.startsWith('success') ? (
+            <p className="success-message">{registrationStatus.substr(9)}</p>
+          ) : null}
+          <Button variant="secondary" onClick={handleModalClose} className="close-button">
+            Закрыть
+          </Button>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
