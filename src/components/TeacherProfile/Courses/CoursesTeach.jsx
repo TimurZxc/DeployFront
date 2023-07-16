c50b1488987f90430d77c6e64c9e4a4c33074624
IDPogolovkin <96434412+IDPogolovkin@users.noreply.github.com>
2023-07-16 22:26:15 +0600

    courses card edit

--- src/components/TeacherProfile/Courses/CoursesTeach.jsx
+++ src/components/TeacherProfile/Courses/CoursesTeach.jsx
@@ -16,22 +16,23 @@ const CoursesTeach = (props) => {
       <div className="first-col">
         <div className="first-row">Предмет</div>
         <div className="second-row">{props.name}</div>
+        <br />
+        <div className="first-row">Язык преподавания</div>
+        <p className="second-row">{props.language}</p>
       </div>
       <div className="second-col">
         <div className="first-row">Описание</div>
         <p className="second-row">{props.description}</p>
+        <br />
         <div className="first-row">Стоимость</div>
         <p className="second-row">{props.price} тг.</p>
       </div>
       <div className="third-col">
         <div className="first-row">Количество мест</div>
         <p className="second-row">{props.number_of_students}</p>
-        
         <br />
-        <div className="first-row">Язык преподавания</div>
-        <p className="second-row">{props.language} тг.</p>
         <div className="first-row">Уровень обучения</div>
-        <p className="second-row">{props.student_level} тг.</p>
+        <p className="second-row">{props.student_level}</p>
 
       <br />
         <button onClick={() => routeHandler(`/subjectsByHours/${props.course_id}`)} className="second-row_t_c">Подробнее</button>
