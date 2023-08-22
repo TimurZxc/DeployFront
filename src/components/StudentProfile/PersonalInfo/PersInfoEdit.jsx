b3f66788056bc3a311ab90723349923c9f182be1
IDPogolovkin <96434412+IDPogolovkin@users.noreply.github.com>
2023-08-22 20:50:09 +0600

    test new camera icon

--- src/components/StudentProfile/PersonalInfo/PersInfoEdit.jsx
+++ src/components/StudentProfile/PersonalInfo/PersInfoEdit.jsx
@@ -311,7 +311,7 @@ const PersonalInfoEdit = (props) => {
             <img className='puple-teach-img' src={props.image ? props.image : userpic} alt="image was not found" crossOrigin="anonymous" />
             <div className="round" type='button'>
               <div onClick={handleOpenModal} type='button' className='photoButt'>
-                <Sprite id='camera' className='icon'/>
+                <Sprite id='camera_stud' className='icon'/>
               </div>
             </div>
           </div>
